bb47f57c0f52c97240adb846bdca911d42ce7494
Roger Johnson <rogermct@hotmail.com>
2025-02-11 15:21:47 -0500

    BaseToTest

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,37 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subSystems.subSwerve;
+import swervelib.SwerveInputStream;
+
+public class RobotContainer {
+  private final CommandXboxController driverOne = new CommandXboxController(0);
+  private subSwerve swerve = new subSwerve();
+  SwerveInputStream driveAngularVelocity = SwerveInputStream.of(swerve.getSwerveDrive(), 
+                                                                () -> driverOne.getLeftY() * -1, 
+                                                                () -> driverOne.getLeftX() * -1)
+                                                                .withControllerRotationAxis(driverOne::getRightX)
+                                                                .deadband(0.05)
+                                                                .scaleTranslation(0.5)
+                                                                .allianceRelativeControl(true);
+
+  SwerveInputStream driveDirectAngle = driveAngularVelocity.copy().withControllerHeadingAxis(driverOne::getRightX,
+                                                                                            driverOne::getRightY)
+                                                                                            .headingWhile(true);
+  public RobotContainer() {
+    DriverOneControls();
+  }
+    
+  private void DriverOneControls() {
+    Command driveFieldOrientatedDirectAngle = swerve.driveFieldOrientated(driveDirectAngle);
+    Command driveFieldOrientatedAngularVelocity = swerve.driveFieldOrientated(driveAngularVelocity);
+
+    swerve.setDefaultCommand(driveFieldOrientatedAngularVelocity);
+  }
+    
+  public Command getAutonomousCommand() {
+    return Commands.print("No autonomous command configured");
+  }
+}
---
+++ src/main/java/frc/robot/subSystems/subSwerve.java
@@ -0,0 +1,60 @@
+package frc.robot.subSystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.io.File;
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj.Filesystem;
+import swervelib.parser.SwerveParser;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
+import swervelib.SwerveDrive;
+import swervelib.SwerveInputStream;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.util.Units;
+import static edu.wpi.first.units.Units.Meter;
+
+public class subSwerve extends SubsystemBase {
+  private final double kMaxSpeed = Units.feetToMeters(19.3);
+  private final SwerveDrive swerveDrive;
+  private final File directory = new File(Filesystem.getDeployDirectory(), "swerve");
+  private final AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
+
+  public subSwerve() {
+    SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+    try
+    {
+      swerveDrive = new SwerveParser(directory).createSwerveDrive(kMaxSpeed,
+                                                                  new Pose2d(new Translation2d(Meter.of(1),
+                                                                                               Meter.of(4)),
+                                                                             Rotation2d.fromDegrees(0)));
+    } catch (Exception e)
+    {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public void periodic() {
+  }
+
+  public SwerveDrive getSwerveDrive() {
+      return swerveDrive;
+  }
+
+  public void driveFieldOrientated(ChassisSpeeds speeds) {
+    swerveDrive.driveFieldOriented(speeds);
+  }
+
+  public Command driveFieldOrientated(Supplier<ChassisSpeeds> chassisSpeeds) {
+    return run(() -> {  
+      driveFieldOrientated(chassisSpeeds.get());
+    });
+  }
+}
